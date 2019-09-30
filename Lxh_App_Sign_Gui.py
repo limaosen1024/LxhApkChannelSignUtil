ffde6c071e013f13b8e7307dd2b6c530e2b5830e
lixiaohui <lixiaohui8636@163.com>
2019-09-30 11:25:06 +0800

    [完善签名保存和日志输出]

--- Lxh_App_Sign_Gui.py
+++ Lxh_App_Sign_Gui.py
@@ -5,14 +5,23 @@ from Util import *
 
 myWin = Tk()
 
-apkPath = StringVar()
+old_apk_path_var = StringVar()
 
-signFilePath = StringVar()
-signPwd = StringVar()
-signAlias = StringVar()
+keystore_file_path_var = StringVar()
+keystore_pwd_var = StringVar()
+keystore_alias_var = StringVar()
 
-allChannel = StringVar()
-outPutDir = StringVar()
+channel_list_var = StringVar()
+out_put_dir_var = StringVar()
+save_keystore = IntVar()
+save_keystore.set(1)
+
+keystore_file_path, keystore_pwd, keystore_alias = readKeyStore()
+
+if (len(keystore_file_path) != 0):
+    keystore_file_path_var.set(keystore_file_path)
+    keystore_pwd_var.set(keystore_pwd)
+    keystore_alias_var.set(keystore_alias)
 
 # 标题
 myWin.title("欢迎使用Apk签名工具")
@@ -22,63 +31,67 @@ myWin.geometry("720x720")
 
 def selectApkPath():
     apk_path_ = askopenfilename()
-    apkPath.set(apk_path_)
+    old_apk_path_var.set(apk_path_)
 
 
 # 选择apk文件
 Label(myWin, text="apk路径：").grid(row=0, column=0)
-Entry(myWin, textvariable=apkPath).grid(row=0, column=1)
+Entry(myWin, textvariable=old_apk_path_var).grid(row=0, column=1)
 Button(myWin, text="选择", command=selectApkPath).grid(row=0, column=2)
 
 
 def selectSignFilePath():
     sign_path_ = askopenfilename()
-    signFilePath.set(sign_path_)
+    keystore_file_path_var.set(sign_path_)
 
 
 # 签名信息
 Label(myWin, text="签名文件路径：").grid(row=1, column=0)
-Entry(myWin, textvariable=signFilePath).grid(row=1, column=1)
+Entry(myWin, textvariable=keystore_file_path_var).grid(row=1, column=1)
 Button(myWin, text="选择", command=selectSignFilePath).grid(row=1, column=2)
 # 签名密码配置
 Label(myWin, text="签名密码：").grid(row=2, column=0)
-Entry(myWin, textvariable=signPwd).grid(row=2, column=1)
+Entry(myWin, textvariable=keystore_pwd_var, show='*').grid(row=2, column=1)
 
 Label(myWin, text="签名别名：").grid(row=3, column=0)
-Entry(myWin, textvariable=signAlias).grid(row=3, column=1)
+Entry(myWin, textvariable=keystore_alias_var).grid(row=3, column=1)
 
-Label(myWin, text="渠道列表：").grid(row=4, column=0)
-Entry(myWin, textvariable=allChannel).grid(row=4, column=1)
+Label(myWin, text="本地保存签名：").grid(row=4, column=0)
+Checkbutton(myWin, variable=save_keystore).grid(row=4, column=1)
+
+Label(myWin, text="渠道列表：").grid(row=5, column=0)
+Entry(myWin, textvariable=channel_list_var).grid(row=5, column=1)
 
 
 def selectOutPath():
     apk_path_ = askdirectory()
-    outPutDir.set(apk_path_)
+    out_put_dir_var.set(apk_path_)
 
 
 # 选择apk文件
-Label(myWin, text="输出路径：").grid(row=5, column=0)
-Entry(myWin, textvariable=outPutDir).grid(row=5, column=1)
-Button(myWin, text="选择", command=selectOutPath).grid(row=5, column=2)
+Label(myWin, text="输出路径：").grid(row=6, column=0)
+Entry(myWin, textvariable=out_put_dir_var).grid(row=6, column=1)
+Button(myWin, text="选择", command=selectOutPath).grid(row=6, column=2)
 
 
 def clickSubmit():
-    oldApkPath = str(apkPath.get())
-    signFilePathR = str(signFilePath.get())
-    signPwdR = str(signPwd.get())
-    signAliasName = str(signAlias.get())
-    outPutDirR = str(outPutDir.get())
-    channel_list = str(allChannel.get()).split(";")
-
-    print("apkPath:" + oldApkPath)
-    print("signFilePath:" + signFilePathR)
-    print("signPwd:" + signPwdR)
-    print("signAlias:" + signAliasName)
-    print("outPutDirR" + outPutDirR)
+    old_apk_path = str(old_apk_path_var.get())
+    keystore_file_path = str(keystore_file_path_var.get())
+    keystore_pwd = str(keystore_pwd_var.get())
+    keystore_alias = str(keystore_alias_var.get())
+    out_put_dir = str(out_put_dir_var.get())
+    channel_list = str(channel_list_var.get()).split(";")
+    print("开始执行....")
+    print("old_apk_path:" + old_apk_path)
+    print("keystore_file_path:" + keystore_file_path)
+    print("keystore_pwd:" + keystore_pwd)
+    print("keystore_alias:" + keystore_alias)
+    print("out_put_dir" + out_put_dir)
     print("channel_list：" + "".join(channel_list))
+    if (save_keystore.get() == 1):
+        saveKeyStore(keystore_file_path, keystore_pwd, keystore_alias)
     # channelApk(oldApkPath, channel_list, outPutDir, signFilePath, signPwdR, signAliasName)
-    saveKeyStore(signFilePathR,signPwdR,signAliasName)
 
 
-Button(myWin, text="确定", command=clickSubmit).grid(row=6, column=0)
+Button(myWin, text="确定", command=clickSubmit).grid(row=7, column=0)
 myWin.mainloop()
--- Util.py
+++ Util.py
@@ -14,6 +14,7 @@ def signApk(temp_unsign_apk_dir, unsign_apk_file_name, out_sign_file_dir, keysto
     os.system(sign_v1_cmd)
     check_v1_sign_cmd = r'jarsigner -verify -verbose %s' % (v1_sign_apk_path)
     os.system(check_v1_sign_cmd)
+    print("V1签名成功："+v1_sign_apk_path)
     # zipalign
     if not os.path.exists(out_sign_file_dir):
         os.makedirs(out_sign_file_dir)
@@ -21,31 +22,33 @@ def signApk(temp_unsign_apk_dir, unsign_apk_file_name, out_sign_file_dir, keysto
     v1_sign_zip_apk_path = out_sign_file_dir + "/" + unsign_file_name_list[0] + "_sign_zip" + unsign_file_name_list[1]
     zip_align_cmd = r'zipalign -v 4 %s %s' % (v1_sign_apk_path, v1_sign_zip_apk_path)
     os.system(zip_align_cmd)
+    print("zipalign成功："+v1_sign_zip_apk_path)
     # v2签名
     sign_v2_cmd = 'apksigner sign --ks %s --ks-pass pass:%s --ks-key-alias %s %s' % (
         keystore_path, keystore_pwd, keystore_alias_name, v1_sign_zip_apk_path)
     os.system(sign_v2_cmd)
+    print("v2签名成功："+v1_sign_zip_apk_path)
 
 
 # 备份manifest文件
 def backUpManifest(back_manifest_dir, temp_manifest_path):
     back_up_manifest_file_path = back_manifest_dir + "/AndroidManifest.xml"
-    print("back_manifest:" + back_up_manifest_file_path)
-    print("temp_manifest:" + temp_manifest_path)
     # 如果存在删除以前的文件
     if os.path.exists(back_manifest_dir):
-        print("删除：" + back_manifest_dir)
+        print("删除备份manifest：" + back_manifest_dir)
         shutil.rmtree(back_manifest_dir)
     os.makedirs(back_manifest_dir)
+    print("备份manifest:" + back_up_manifest_file_path)
     # 复制temp到back的路径
     shutil.copy(temp_manifest_path, back_up_manifest_file_path)
+    print("备份manifest成功:" + back_up_manifest_file_path)
     return back_up_manifest_file_path
 
 
 def modifyChannel(channel, back_up_manifest_file_path, temp_manifest_path, unzip_temp_dir, temp_dir, apk_file,
                   out_sign_file_dir, keystore_path, keystore_pwd, keystore_alias_name):
     android_ns = 'http://schemas.android.com/apk/res/android'
-    print(channel)
+    print("修改的渠道为："+channel)
     # 替换channel value
     ET.register_namespace('android', android_ns)
     tree = ET.parse(back_up_manifest_file_path)
@@ -70,10 +73,10 @@ def modifyChannel(channel, back_up_manifest_file_path, temp_manifest_path, unzip
     unsign_apk_file_name = file_name_list[0] + "_" + channel + "_unsigned" + file_name_list[1]
     # 生成未签名的apk
     unsign_apk_file_path = temp_unsign_apk_dir + "/" + unsign_apk_file_name
-    print("生成未签名apk:" + unsign_apk_file_path)
     apk_tool_unsign_apk_cmd = r'java -jar apktool_2.4.0.jar -p %s b %s -o %s' % (
         "./framework", unzip_temp_dir, unsign_apk_file_path)
     os.system(apk_tool_unsign_apk_cmd)
+    print("生成未签名apk:" + unsign_apk_file_path)
     # 签名
     signApk(temp_unsign_apk_dir, unsign_apk_file_name, out_sign_file_dir, keystore_path, keystore_pwd,
             keystore_alias_name)
@@ -81,54 +84,64 @@ def modifyChannel(channel, back_up_manifest_file_path, temp_manifest_path, unzip
 
 def channelApk(apk_file, channel_list, out_sign_file_dir, keystore_path, keystore_pwd, keystore_alias_name):
     print("开始多渠道打包，原Apk路径为：" + apk_file)
-    apk_dir = os.path.dirname(apk_file)
     temp_dir = "./temp"
     if os.path.exists(temp_dir):
+        print("删除老的临时文件目录：") + os.path.realpath(temp_dir)
         shutil.rmtree(temp_dir)
     os.makedirs(temp_dir)
+    print("临时文件目录为：") + os.path.realpath(temp_dir)
     # 解压的文件路径
     unzip_temp_dir = temp_dir + "/channelTemp"
-    print(unzip_temp_dir)
     # 判断是否有文件，否则删除
     if os.path.exists(unzip_temp_dir):
-        print("删除：" + unzip_temp_dir)
+        print("删除老的解压目录：" + unzip_temp_dir)
         shutil.rmtree(unzip_temp_dir)
     # 创建临时目录
     os.makedirs(unzip_temp_dir)
+    print("apk解压目录为：") + os.path.realpath(unzip_temp_dir)
 
     # apktool解压apk
     apk_tool_unzip_cmd = r'java -jar apktool_2.4.0.jar -p %s d -f -s %s  -o %s' % (
         "./framework", apk_file, unzip_temp_dir)
     os.system(apk_tool_unzip_cmd)
+    print("apk解压成功：") + os.path.realpath(unzip_temp_dir)
     # 得到解压的manifest文件路径
     temp_manifest_path = unzip_temp_dir + "/AndroidManifest.xml"
+    print("解压的Manifest文件地址：") + os.path.realpath(unzip_temp_dir)
     # 备份一份到
     back_manifest_dir = temp_dir + "/manifestBackUp"
     back_up_manifest_file_path = backUpManifest(back_manifest_dir, temp_manifest_path)
+    print("开始修改渠道:" + "".join(channel_list, "\n"))
     # 去执行每一个
     for channel in channel_list:
         modifyChannel(channel, back_up_manifest_file_path, temp_manifest_path, unzip_temp_dir, temp_dir, apk_file,
                       out_sign_file_dir, keystore_path, keystore_pwd, keystore_alias_name)
 
-    print("success")
+    print("全部完成~~~~")
 
 
-keystore_file_path = "./keystore_config.txt"
+keystore_config_file_path = "./keystore_config.txt"
 
 
 def saveKeyStore(keystore_path, keystore_pass, keystore_alias):
     oldPath, oldPass, oldAlias = readKeyStore()
-    newCon = keystore_path+"\n"+keystore_pass+"\n"+keystore_alias
-    oldCon = oldPath+"\n"+oldPass+"\n"+oldAlias
+    newCon = keystore_path + "\n" + keystore_pass + "\n" + keystore_alias
+    oldCon = oldPath + "\n" + oldPass + "\n" + oldAlias
     if oldCon == newCon:
         return
-    if os.path.exists(keystore_file_path):
-        os.remove(keystore_file_path)
-    fi = open(keystore_file_path, "w", encoding="utf-8")
+    if os.path.exists(keystore_config_file_path):
+        os.remove(keystore_config_file_path)
+    print("保存签名配置成功")
+    fi = open(keystore_config_file_path, "w", encoding="utf-8")
     fi.write(newCon)
 
+
 def readKeyStore():
-    fi = open(keystore_file_path, "r", encoding="utf-8")
-    cont = fi.read()
-    cont_list = cont.split("\n")
-    return cont_list[0], cont_list[1], cont_list[2]
+    if (os.path.exists(keystore_config_file_path)):
+        fi = open(keystore_config_file_path, "r", encoding="utf-8")
+        cont = fi.read()
+        cont_list = cont.split("\n")
+        print("读取签名配置成功")
+        return cont_list[0], cont_list[1], cont_list[2]
+    else:
+        return "", "", ""
